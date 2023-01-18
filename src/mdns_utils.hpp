27850e68b52dbad23b8522a1c4e78ffdd942972a
Joel Leow <woeltic@gmail.com>
2023-01-19 00:31:13 +0800

    Bugfix for windows

--- src/mdns_utils.hpp
+++ src/mdns_utils.hpp
@@ -6,6 +6,7 @@
 
 #include <functional>
 #include <string>
+#include <array>
 #include <memory>
 #include <vector>
 
@@ -32,15 +33,19 @@ namespace mdns_cpp
 
 enum
 {
+#if defined(__linux__) // On windows IFF_UP, BROADCAST, LOOPBACK, POINTTOPOINT, MULTICAST are already defined??
     IFF_UP = 0x1,		/* Interface is up.  */
 # define IFF_UP	IFF_UP
     IFF_BROADCAST = 0x2,	/* Broadcast address valid.  */
 # define IFF_BROADCAST	IFF_BROADCAST
+#endif
     IFF_DEBUG = 0x4,		/* Turn on debugging.  */
 # define IFF_DEBUG	IFF_DEBUG
+#if defined(__linux__)
     IFF_LOOPBACK = 0x8,		/* Is a loopback net.  */
 # define IFF_LOOPBACK	IFF_LOOPBACK
     IFF_POINTOPOINT = 0x10,	/* Interface is point-to-point link.  */
+#endif
 # define IFF_POINTOPOINT IFF_POINTOPOINT
     IFF_NOTRAILERS = 0x20,	/* Avoid use of trailers.  */
 # define IFF_NOTRAILERS	IFF_NOTRAILERS
@@ -59,10 +64,10 @@ enum
 # define IFF_MASTER	IFF_MASTER
     IFF_SLAVE = 0x800,		/* Slave of a load balancer.  */
 # define IFF_SLAVE	IFF_SLAVE
-
+#if defined(__linux__)
     IFF_MULTICAST = 0x1000,	/* Supports multicast.  */
 # define IFF_MULTICAST	IFF_MULTICAST
-
+#endif
     IFF_PORTSEL = 0x2000,	/* Can set media type.  */
 # define IFF_PORTSEL	IFF_PORTSEL
     IFF_AUTOMEDIA = 0x4000,	/* Auto media select active.  */
@@ -143,19 +148,17 @@ inline OpenSocketsData OpenClientSockets(int port, std::size_t max_sockets = 64)
 	// When sending, each socket can only send to one network interface
 	// Thus we need to open one socket for each interface and address family
 
-	int has_ipv4;
-	int has_ipv6;
 #ifdef _WIN32
-    auto adapter_address = std::make_unique<IP_ADAPTER_ADDRESSES>();
+	IP_ADAPTER_ADDRESSES* adapter_address = 0;
 	ULONG address_size = 8000;
 	unsigned int ret;
 	unsigned int num_retries = 4;
 	do {
 		adapter_address = (IP_ADAPTER_ADDRESSES*)malloc(address_size);
-		ret = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_ANYCAST, 0,
-		                           adapter_address.get(), &address_size);
+		ret = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_ANYCAST, NULL,
+		                           adapter_address, &address_size);
 		if (ret == ERROR_BUFFER_OVERFLOW) {
-			adapter_address.reset();
+			free(adapter_address);
 			address_size *= 2;
 		} else {
 			break;
@@ -163,14 +166,14 @@ inline OpenSocketsData OpenClientSockets(int port, std::size_t max_sockets = 64)
 	} while (num_retries-- > 0);
 
 	if (!adapter_address || (ret != NO_ERROR)) {
-        adapter_address.reset();
+		free(adapter_address);
 		Log(LogLevel::Warn, "Failed to get network adapter addresses");
-		return num_sockets;
+		return returnData;
 	}
 
 	int first_ipv4 = 1;
 	int first_ipv6 = 1;
-	for (PIP_ADAPTER_ADDRESSES adapter = adapter_address.get(); adapter; adapter = adapter->Next) {
+	for (PIP_ADAPTER_ADDRESSES adapter = adapter_address; adapter; adapter = adapter->Next) {
 		if (adapter->TunnelType == TUNNEL_TYPE_TEREDO)
 			continue;
 		if (adapter->OperStatus != IfOperStatusUp)
@@ -184,23 +187,19 @@ inline OpenSocketsData OpenClientSockets(int port, std::size_t max_sockets = 64)
 				    (saddr->sin_addr.S_un.S_un_b.s_b2 != 0) ||
 				    (saddr->sin_addr.S_un.S_un_b.s_b3 != 0) ||
 				    (saddr->sin_addr.S_un.S_un_b.s_b4 != 1)) {
-					int log_addr = 0;
 					if (first_ipv4) {
 						returnData.service_address_ipv4 = *saddr;
 						first_ipv4 = 0;
-						log_addr = 1;
 					}
-					has_ipv4 = 1;
-					if (sockets.size() < max_sockets) {
+					if (returnData.sockets.size() < max_sockets) {
 						saddr->sin_port = htons((unsigned short)port);
 						int sock = mdns_socket_open_ipv4(saddr);
+						const auto addr = IPV4AddressToString(saddr, sizeof(struct sockaddr_in));
 						if (sock >= 0) {
 							returnData.sockets.push_back(sock);
-							log_addr = 1;
-							const auto addr = IPV4AddressToString(saddr, sizeof(struct sockaddr_in));
-                        	Log(LogLevel::Debug, "Socket opened for interface with local IPv6 address: " + addr);
+                        	Log(LogLevel::Debug, "Socket opened for interface with local IPv4 address: " + addr);
 						} else {
-							log_addr = 0;
+							Log(LogLevel::Debug, "Failed to open interface with local IPv4 address: " + addr);
 						}
 					}
 				}
@@ -216,30 +215,26 @@ inline OpenSocketsData OpenClientSockets(int port, std::size_t max_sockets = 64)
 				if ((unicast->DadState == NldsPreferred) &&
 				    memcmp(saddr->sin6_addr.s6_addr, localhost, 16) &&
 				    memcmp(saddr->sin6_addr.s6_addr, localhost_mapped, 16)) {
-					int log_addr = 0;
 					if (first_ipv6) {
 						returnData.service_address_ipv6 = *saddr;
 						first_ipv6 = 0;
-						log_addr = 1;
 					}
-					has_ipv6 = 1;
-					if (sockets.size() < max_sockets) {
+					if (returnData.sockets.size() < max_sockets) {
 						saddr->sin6_port = htons((unsigned short)port);
 						int sock = mdns_socket_open_ipv6(saddr);
+						const auto addr = IPV6AddressToString(saddr, sizeof(struct sockaddr_in6));
 						if (sock >= 0) {
 							returnData.sockets.push_back(sock);
-							log_addr = 1;
-							const auto addr = IPV6AddressToString(saddr, sizeof(struct sockaddr_in6));
 							Log(LogLevel::Debug, "Socket opened for interface with local IPv6 address: " + addr);
 						} else {
-							log_addr = 0;
+							Log(LogLevel::Debug, "Failed to open interface with local IPv6 address: " + addr);
 						}
 					}
 				}
 			}
 		}
 	}
-
+	free(adapter_address);
 
 #else
 
@@ -263,23 +258,17 @@ inline OpenSocketsData OpenClientSockets(int port, std::size_t max_sockets = 64)
 		if (ifa->ifa_addr->sa_family == AF_INET) {
 			struct sockaddr_in* saddr = (struct sockaddr_in*)ifa->ifa_addr;
 			if (saddr->sin_addr.s_addr != htonl(INADDR_LOOPBACK)) {
-				int log_addr = 0;
 				if (first_ipv4) {
 					returnData.service_address_ipv4 = *saddr;
 					first_ipv4 = 0;
-					log_addr = 1;
 				}
-				has_ipv4 = 1;
 				if (returnData.sockets.size() < max_sockets) {
 					saddr->sin_port = htons(port);
 					int sock = mdns_socket_open_ipv4(saddr);
 					if (sock >= 0) {
 						returnData.sockets.push_back(sock);
-						log_addr = 1;
 						const auto addr = IPV4AddressToString(saddr, sizeof(struct sockaddr_in));
                     	Log(LogLevel::Debug, "Socket opened for interface with local IPv4 address: " + addr);
-					} else {
-						log_addr = 0;
 					}
 				}
 			}
@@ -294,23 +283,17 @@ inline OpenSocketsData OpenClientSockets(int port, std::size_t max_sockets = 64)
 			                                                 0, 0, 0xff, 0xff, 0x7f, 0, 0, 1};
 			if (memcmp(saddr->sin6_addr.s6_addr, localhost, 16) &&
 			    memcmp(saddr->sin6_addr.s6_addr, localhost_mapped, 16)) {
-				int log_addr = 0;
 				if (first_ipv6) {
 					returnData.service_address_ipv6 = *saddr;
 					first_ipv6 = 0;
-					log_addr = 1;
 				}
-				has_ipv6 = 1;
 				if (returnData.sockets.size() < max_sockets) {
 					saddr->sin6_port = htons(port);
 					int sock = mdns_socket_open_ipv6(saddr);
 					if (sock >= 0) {
 						returnData.sockets.push_back(sock);
-						log_addr = 1;
 						const auto addr = IPV6AddressToString(saddr, sizeof(struct sockaddr_in6));
                     	Log(LogLevel::Debug, "Socket opened for interface with local IPv6 address: " + addr);
-					} else {
-						log_addr = 0;
 					}
 				}
 			}
@@ -461,8 +444,7 @@ inline int ServiceCallback(int sock, const struct sockaddr* from, size_t addrlen
 	const char dns_sd[] = "_services._dns-sd._udp.local.";
 	const service_t* service = (const service_t*)user_data;
 
-	const std::string fromaddrstr_ = IPAddressToString(from, addrlen);
-	mdns_string_t fromaddrstr = Convert(fromaddrstr_);
+	const std::string fromaddrstr_cpp = IPAddressToString(from, addrlen);
 
 	char namebuffer[256];
 	size_t offset = name_offset;
@@ -497,7 +479,7 @@ inline int ServiceCallback(int sock, const struct sockaddr* from, size_t addrlen
 	else
 		return 0;
 
-	Log(LogLevel::Info, fmt::format("Query {} {}", record_name, std::string(name.str, name.length)));
+	Log(LogLevel::Info, fmt::format("{} - Query {} {}", fromaddrstr_cpp, record_name, std::string(name.str, name.length)));
 
 	std::array<char, 1024> sendbuffer;
 	if ((name.length == (sizeof(dns_sd) - 1)) &&
@@ -631,7 +613,6 @@ inline int ServiceCallback(int sock, const struct sockaddr* from, size_t addrlen
 			uint16_t unicast = (rclass & MDNS_UNICAST_RESPONSE);
 			std::string addrstr_cpp = IPAddressToString((struct sockaddr*)&service->record_a.data.a.addr,
 			    sizeof(service->record_a.data.a.addr));
-			mdns_string_t addrstr = Convert(addrstr_cpp);
 
 			Log(LogLevel::Info, fmt::format("  --> answer  {} IPv4 {} ({})", std::string(service->record_a.name.str, service->record_a.name.length), addrstr_cpp, (unicast ? "unicast" : "multicast")));
 
@@ -667,9 +648,7 @@ inline int ServiceCallback(int sock, const struct sockaddr* from, size_t addrlen
 
 			std::string addrstr_cpp = IPAddressToString((struct sockaddr*)&service->record_aaaa.data.aaaa.addr,
 			    sizeof(service->record_aaaa.data.aaaa.addr));
-
-			mdns_string_t addrstr = Convert(addrstr_cpp);
-			
+		
 			Log(LogLevel::Info, fmt::format("  --> answer  {} IPv6 {} ({})", std::string(service->record_aaaa.name.str, service->record_aaaa.name.length), addrstr_cpp, (unicast ? "unicast" : "multicast")));
 
 			if (unicast) {
